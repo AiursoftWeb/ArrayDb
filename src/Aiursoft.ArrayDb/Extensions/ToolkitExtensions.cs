74798c53105ce9576ce5bfaf926a23271eed465c
Anduin Xue <anduin@aiursoft.com>
2024-10-26 07:23:20 +0000

    Remove unnecessary benchmarking code
    The `ShowBench` flag and related conditionals in `ToolkitExtensions.cs` were removed to simplify the `RunWithTimedBench` method. This change eliminates redundant checks and ensures the method always measures and returns execution time.

--- src/Aiursoft.ArrayDb/Extensions/ToolkitExtensions.cs
+++ src/Aiursoft.ArrayDb/Extensions/ToolkitExtensions.cs
@@ -4,22 +4,8 @@ namespace Aiursoft.ArrayDb.Extensions;
 
 public static class ToolkitExtensions
 {
-    // ReSharper disable once FieldCanBeMadeReadOnly.Global
-    public static bool ShowBench = true;
-    
     public static TimeSpan RunWithTimedBench(string actionName, Action action)
     {
-        if (!ShowBench)
-        {
-            action();
-            return TimeSpan.Zero;
-        }
-        
-        if (string.IsNullOrWhiteSpace(actionName))
-        {
-            actionName = "Anonymous action";
-        }
-        
         var stopwatch = new Stopwatch();
         stopwatch.Start();
         action();
