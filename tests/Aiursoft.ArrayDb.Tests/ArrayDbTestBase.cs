a9e2b949a76763e7f58e64732de2ed7eb288834b
Anduin Xue <anduin@aiursoft.com>
2024-10-27 16:38:23 +0000

    Add synchronization to ArrayDb test setup and cleanup
    Introduced a lock object to ensure thread safety during test initialization and cleanup. This prevents potential race conditions when tests are accessing and modifying shared files.

--- tests/Aiursoft.ArrayDb.Tests/ArrayDbTestBase.cs
+++ tests/Aiursoft.ArrayDb.Tests/ArrayDbTestBase.cs
@@ -5,10 +5,20 @@ namespace Aiursoft.ArrayDb.Tests;
 [TestClass]
 public abstract class ArrayDbTestBase
 {
+    public static readonly object LockObject = new();
+
     [TestInitialize]
     public void Init()
     {
         File.Delete("sampleData.bin");
         File.Delete("sampleDataStrings.bin");
+        Monitor.Enter(LockObject);
+
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        Monitor.Exit(LockObject);
     }
 }
