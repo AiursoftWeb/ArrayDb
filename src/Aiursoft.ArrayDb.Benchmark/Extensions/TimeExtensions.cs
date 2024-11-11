8587e4d01dea886bf02f614206182253edf841c9
Anduin Xue <anduin@aiursoft.com>
2024-11-11 05:59:11 +0000

    Refactor `RunTest` method to include warm-up and average runs
    Revised the `RunTest` method to perform two warm-up executions followed by three test executions to calculate the average duration. Introduced a private method `RunOneTest` to modularize and simplify the execution logic.

--- src/Aiursoft.ArrayDb.Benchmark/Extensions/TimeExtensions.cs
+++ src/Aiursoft.ArrayDb.Benchmark/Extensions/TimeExtensions.cs
@@ -13,41 +13,28 @@ public enum ActionBeforeTestings
 
 public static class TimeExtensions
 {
-    public static async Task<TimeSpan> RunTest(TestTarget testTarget, Func<IObjectBucket<TestEntity>, Task> action, ActionBeforeTestings actionBefore = ActionBeforeTestings.None)
+    public static async Task<TimeSpan> RunTest(TestTarget testTarget, Action<IObjectBucket<TestEntity>> action, ActionBeforeTestings actionBefore = ActionBeforeTestings.None)
     {
-        // get
-        var target = testTarget.TestEntities();
-
-        // Prepare
-        switch (actionBefore)
+        // Warm up twice
+        _ = await RunOneTest(testTarget, action, actionBefore);
+        _ = await RunOneTest(testTarget, action, actionBefore);
+        
+        // Test 3 times, get average
+        var testResult1 = await RunOneTest(testTarget, action, actionBefore);
+        var testResult2 = await RunOneTest(testTarget, action, actionBefore);
+        var testResult3 = await RunOneTest(testTarget, action, actionBefore);
+        var average = new[]
         {
-            case ActionBeforeTestings.Insert1KItemsBeforeTesting:
-            {
-                var dataToAdd = TestEntityFactory.CreateSome(Program.OneKilo);
-                target.Add(dataToAdd);
-                break;
-            }
-            case ActionBeforeTestings.Insert1MItemsBeforeTesting:
-            {
-                var dataToAdd = TestEntityFactory.CreateSome(Program.OneMillion);
-                target.Add(dataToAdd);
-                break;
-            }
-        }
+            testResult1,
+            testResult2,
+            testResult3
+        }.Average(t => t.TotalMilliseconds);
         
-        // Test
-        var sw = new Stopwatch();
-        sw.Start();
-        await action(target);
-        sw.Stop();
-        
-        // Clean
-        await target.DeleteAsync();
-        await Task.Delay(2000);
-        return sw.Elapsed;
+        // Return average
+        return TimeSpan.FromMilliseconds(average);
     }
     
-    public static async Task<TimeSpan> RunTest(TestTarget testTarget, Action<IObjectBucket<TestEntity>> action, ActionBeforeTestings actionBefore = ActionBeforeTestings.None)
+    private static async Task<TimeSpan> RunOneTest(TestTarget testTarget, Action<IObjectBucket<TestEntity>> action, ActionBeforeTestings actionBefore = ActionBeforeTestings.None)
     {
         // get
         var target = testTarget.TestEntities();
